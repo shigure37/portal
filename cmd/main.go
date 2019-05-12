9cacb5baa3e2a2591d97320ded96ac1627f3ecc7
shigure37 <shigure3788@gmail.com>
2019-05-12 20:40:27 +0900

    workspace init

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+/*================================================================
+*   FileName    : main.go
+*   Author      : mavinyeh
+*   Date        : 2019-05-12
+*   Description :
+================================================================*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("this is a test")
+}
